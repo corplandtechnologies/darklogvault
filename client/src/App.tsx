9b18b62abfcdbd1e0aa80a0ec92ea045f96cc181
asantesylvesterkwame <intelligentson17@gmail.com>
2024-07-04 21:04:25 +0000

    fixed routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,66 +1,54 @@
+// Import necessary hooks and components
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Register from "./Pages/Register";
 import Login from "./Pages/Login";
 import { Toaster } from "react-hot-toast";
 import Dashboard from "./Pages/Dashboard";
 import Deposit from "./Pages/Deposit";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 
+// Custom hook for protected routes
+const useProtectedRoute = (navigate: ReturnType<typeof useNavigate>) => {
+  const currentUser = JSON.parse(localStorage.getItem("user") || "{}");
+
+  useEffect(() => {
+    if (!currentUser) {
+      navigate("/login");
+    }
+  }, [currentUser, navigate]);
+};
+
 const App = () => {
-  const [currentUser, setCurrentUser] = useState<object>({});
   const navigate = useNavigate();
 
-useEffect(() => {
-  const storedUserString = localStorage.getItem("user");
-  let user: object;
-
-  if (storedUserString !== null) {
-    user = JSON.parse(storedUserString);
-  } else {
-    // Handle the case where there is no user data in local storage
-    // For example, set user to an empty object or null
-    user = {};
-  }
-
-  setCurrentUser(user);
-  if (!user) {
-    return navigate("/login");
-  }
-}, []);
-
-  const isAuthPage = () => {
-    return ["/register", "/login"].includes(window.location.pathname);
-  };
+  // Use the custom hook for protected routes
+  useProtectedRoute(navigate);
 
   return (
     <>
       <Toaster />
-      {!isAuthPage() && <Navbar />}
+      <Navbar />
       <Routes>
-        {currentUser ? (
-          <>
-            <Route
-              path="/"
-              element={<Dashboard />}
-            />
-            <Route
-              path="/deposit"
-              element={<Deposit />}
-            />
-          </>
-        ) : (
-          <>
-            <Route
-              path="/register"
-              element={<Register />}
-            />
-            <Route
-              path="/login"
-              element={<Login />}
-            />
-          </>
-        )}
+        {/* Protected routes */}
+        <Route
+          path="/"
+          element={<Dashboard />}
+        />
+        <Route
+          path="/deposit"
+          element={<Deposit />}
+        />
+
+        {/* Public routes */}
+        <Route
+          path="/register"
+          element={<Register />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
       </Routes>
     </>
   );
