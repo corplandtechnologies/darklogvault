10f06e47794fe636df22531a8618d7e040390e4c
asantesylvesterkwame <intelligentson17@gmail.com>
2024-07-28 01:26:54 +0000

    fixed Cors

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(helmet());
 app.use(morgan("common"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://darklogvaultapp.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
