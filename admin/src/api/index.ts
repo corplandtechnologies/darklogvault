ead48470832c61527fcf466004702851373806c1
asantesylvesterkwame <intelligentson17@gmail.com>
2024-08-26 16:03:12 +0000

    hanged server api

--- admin/src/api/index.ts
+++ admin/src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "https://darklogvault.onrender.com/api/v1",
+  baseURL: "https://darklogvaultbackend.onrender.com/api/v1",
   withCredentials: true,
 });
 
